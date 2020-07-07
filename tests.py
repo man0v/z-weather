d9455c85c7ece3984c018c30e7fd3c8cd62c100c
Georgi Manov <root@lynxsome.com>
2020-07-07 02:41:09 +0100

    add a new ignore; add two test prototypes

--- tests.py
+++ tests.py
@@ -6,5 +6,12 @@ import weather
 class Test_Weather(unittest.TestCase):
     """Main Unit Test class for Weather"""
 
-    def test_config(self):
-        pass
+    def test_grace_exit(self):
+        """Ensure the grace_exit function works as expected"""
+        self.assertEqual(1, 1)
+
+    def test_debug(self):
+        """Test the debug function is printing output if the WEATHER_DEBUG ENV var is set"""
+
+if __name__ == "__main__":
+    unittest.main()
